a0079727cc5c57ca1cc7269047725443525fbeee
istteo <istteogit@gmail.com>
2021-04-25 19:43:35 +0100

    Make .py the agent decision process and use Jupyter notebook as display and notebook for progress

--- Project/RndBot.py
+++ Project/RndBot.py
@@ -1,25 +1,11 @@
-import gym
-import gym_anytrading
-from gym_anytrading.envs import TradingEnv, ForexEnv, StocksEnv, Actions, Positions 
-from gym_anytrading.datasets import FOREX_EURUSD_1H_ASK, STOCKS_GOOGL
-import matplotlib.pyplot as plt
-import pandas as pd
-df = pd.read_csv("FOREX_EURUSD_1H_ASK_DAILY.csv")
-env = gym.make('forex-v0', df=df,frame_bound=(21, 200), window_size=1)
-# env = gym.make('stocks-v0', frame_bound=(50, 100), window_size=10)
+import random
+def rndCide(pastCloses,observation,sold,action):
+    action =random.getrandbits(1)
+    if(sold and action==1):
+        sold = False
+    elif(not sold and action==0):
+        sold = True
 
-obs = []
+    return action, sold
 
-observation = env.reset()
-while True:
-    action = env.action_space.sample()
-    observation, reward, done, info = env.step(action)
-    obs.append({"obs":observation[0]})
-    #env.render()
-    if done:
-        print("info:", info)
-        break
 
-plt.cla()
-env.render_all()
-plt.show()
