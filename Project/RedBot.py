8fe376927d8e2c710c56b56e4c043b30f76592f1
istteo <istteogit@gmail.com>
2021-05-22 18:50:32 +0100

    Permutations of different bots used. Add profit line for comparison.

--- Project/RedBot.py
+++ Project/RedBot.py
@@ -5,18 +5,27 @@ from RLBot2 import *
 from RLBot1 import *
 from RndBot import *
 from TurtleBot import *
+import itertools
+
+botLst = [TurtleBot,RLBot1,RLBot2]
+allCombs = []
+for L in range(2, len(botLst)+1):
+    for subset in itertools.combinations(botLst, L):
+        allCombs.append(subset)
+
 
 class RedBot:
 
-    def __init__(self, bots, wealth):
+    def __init__(self, bots, initWealth):
         print("Hey Komrade!")
         #self.weights = np.random.randn(len(bots))
         self.weights = [.5 for i in range(len(bots))]
-        self.wealth = wealth
+        self.wealth = initWealth
         self.profits = []
         self.bots = bots
         for b in bots:
             self.profits.append(display(b))
+        self.wealthHistory = [initWealth]
 
     def decide(self):
         investments = softmax(self.weights)*self.wealth
@@ -31,10 +40,22 @@ class RedBot:
                 elif(profit <0):
                     self.weights[bI] -= 1
                 investments = softmax(self.weights)*self.wealth
-        print(self.wealth)
+            
+            self.wealthHistory.append(self.wealth)
 
+        
+        return self.wealthHistory
 
-red = RedBot([RndBot, TurtleBot, RLBot1, RLBot2],1000)
-red.decide()
+initWealth = 1000
 
+for comb in allCombs:
+    red = RedBot(comb ,initWealth)
+    wealthHistory = red.decide()
+    xAxis = [i*30 for i in range(len(wealthHistory)) ] 
+    plt.xticks(xAxis)
+    plt.axhline(initWealth,0,360,color='k',linestyle='dashed')
+    plt.title(str(comb))
+    plt.plot(xAxis,wealthHistory)
+    plt.show()
 
+print(allCombs)
